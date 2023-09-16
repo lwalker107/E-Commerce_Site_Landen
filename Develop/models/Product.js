8ec27fc420aa35287fc3b968bb4fe478b462b1a7
lwalker <landen@example.com>
2023-09-16 16:44:23 -0500

    finished Tag, ProductTag and Product models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,18 +9,31 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     // define columns
     product_name: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
+    }, 
     price: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.DECIMAL,
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
