ee4fc208630e9d45d6e11f837db1aece1bc931ec
lwalker <landen@example.com>
2023-09-16 18:50:31 -0500

    product-routes api finished

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,12 +6,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
+    include: [ Category, Tag ]
+  }).then((productData) => {res.json(productData)})
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [ Catagory, Tag ]
+  }).then((productData) => res.json(productData));
   // be sure to include its associated Category and Tag data
 });
 
@@ -91,6 +100,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then((deletedProduct) => {
+    res.json(deletedProduct);
+  }).catch((err) => res.json(err));
 });
 
 module.exports = router;
